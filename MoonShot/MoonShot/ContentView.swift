0417ff535e5bd800f65f2bbf2228d67381c56eed
Tarun Uggina <tarun.uggina@gmail.com>
2026-06-17 17:05:29 +0530

    Resizing images, working with scrollview
    Resizing images, working with scrollview

--- MoonShot/MoonShot/ContentView.swift
+++ MoonShot/MoonShot/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    
+    let text: String
+    
+    var body: some View{
+        Text(text)
+    }
+    
+    init(text: String){
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        //MARK: Resizing images
+//        Image(.example) // give your image name in lowercase, which is present inside the assets
+//            .resizable() //exactly fits into our frame, resizes automatically.
+//            .scaledToFit()
+//            .containerRelativeFrame(.horizontal) { size, axis in
+//                size * 0.8
+//            } //does leave gaps and fills all the frame exactly, even if it is scaledTofit().
+        
+        
+        //MARK: Scrollview, using CustomViews. LazyVstack vs Vstack
+        ScrollView{
+            VStack{
+                ForEach(0..<100) {
+                    CustomText(text:"Item \($0)")
+                        .font(.title)
+                }
+                .frame(maxWidth: .infinity)
+            }
         }
-        .padding()
     }
 }
 
