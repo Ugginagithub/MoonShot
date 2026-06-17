cc1a0b2a7ba95ff9f6e021fa52ea21ba979a6973
UGGINA TARUN PAVAN KUMAR <98165814+Ugginagithub@users.noreply.github.com>
2026-06-17 15:39:56 +0530

    This app is about the moon and astronauts,
    We will be learning about scrollview, navigationStack, List, Form, text, screen navigations, etc.. really excited

---
+++ MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Tarun on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
